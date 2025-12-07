2c59694a26d98f5a31c7263136960f67d4c22002
Aseem Ratha <aaratha@Mac.lan>
2025-12-07 01:34:14 -0500

    added .. aliases for cd

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function ..
+    cd ..
+end
+function ...
+    cd ../..
+end
+function ....
+    cd ../../..
+end
+function .....
+    cd ../../../..
+end
+
+zoxide init fish | source
+starship init fish | source
